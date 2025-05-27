a3c0f88bccac5bff18e2ae809a81c0e6df371f60
MarcosPatron <152577391+MarcosPatron@users.noreply.github.com>
2025-05-27 10:08:59 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,22 @@
 from flask import Flask
 from API.assistants_api import assistants_bp
-# from API.users_api import users_bp
+#from API.users_api import users_bp
 from dotenv import load_dotenv
 import os
 
 def create_app():
+    # Cargar variables de entorno desde .env
     load_dotenv()
+
     app = Flask(__name__)
+
+    # Registrar blueprints
     app.register_blueprint(assistants_bp)
-    # app.register_blueprint(users_bp)
+    #app.register_blueprint(users_bp)
+
     return app
 
-# Este bloque solo corre cuando ejecutas directamente `python run.py`
 if __name__ == "__main__":
     app = create_app()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
-
-# Este bloque corre siempre, y lo usará Gunicorn
-app = create_app()
