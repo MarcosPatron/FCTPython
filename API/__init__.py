34ca01df647a9152559c133dd4c58ddbe7b1153c
MarcosPatron <152577391+MarcosPatron@users.noreply.github.com>
2025-05-28 09:51:48 +0200

    Update __init__.py

--- API/__init__.py
+++ API/__init__.py
@@ -1,1 +1,17 @@
+# api/__init__.py
+
+from .user_api import UserAPI
+from .ticket_api import TicketAPI
+from .assistants_api import assistants_bp
+
+
+def register_blueprints(app):
+    # Instanciar clases con blueprints
+    user_api = UserAPI()
+    ticket_api = TicketAPI()
+
+    # Registrar todos los blueprints en la aplicación
+    app.register_blueprint(user_api.blueprint)
+    app.register_blueprint(ticket_api.blueprint)
+    app.register_blueprint(assistants_bp)
 
