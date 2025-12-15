db85dcaea5316d7f2b14676916fc085ad329beb1
MarcosPatron <152577391+MarcosPatron@users.noreply.github.com>
2025-12-15 23:16:40 +0100

    Errores Railway

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,32 @@
 # wsgi.py
 
-# Se utiliza para ejecutar el proyecto en Render
-from run import create_app
+# Se utiliza para ejecutar el proyecto en Railway
+from flask import Flask
+from dotenv import load_dotenv
+import os
 
+# Importa los blueprints desde la carpeta API
+from API.assistants_api import assistants_bp
+from API.users_api import UserAPI
+from API.tickets_api import TicketAPI
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+
+    user_api = UserAPI()
+    ticket_api = TicketAPI()
+
+    app.register_blueprint(assistants_bp)
+    app.register_blueprint(user_api.blueprint)
+    app.register_blueprint(ticket_api.blueprint)
+
+    return app
+
+# 🔹 App global para Gunicorn
 app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000)) # Puerto de la API
+    app.run(host="0.0.0.0", port=port, debug=True)
+
